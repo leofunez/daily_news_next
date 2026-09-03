6d549d473c902c9c21a9cae9f404a0c643067ad6
Leonardo Funez <leonardofunezdelchiaro@gmail.com>
2026-09-03 15:10:14 -0500

    Use useGetFavoritesQuery redux api instead of fetching data manually

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,86 +1,51 @@
 "use client";
 
-// Hooks
-import { useEffect, useState } from "react";
-
 // Store
 import { useSelector } from 'react-redux';
-import { useAppSelector } from "@/redux/hooks";
 import { useGetFavoritesQuery } from "@/redux/api/favoritesApi";
 import { selectFavorites } from "@/redux/slices/favoritesSlice";
 
-// API
-import fetchWebApi from "@/api/fetchWebApi";
-
 // Types
 import { type JSX } from "react";
-import { PostType } from "@/types/post.types";
 
 // Components
 import SectionTitle from "@/components/ui/SectionTitle/SectionTitle";
 import GridList from "@/components/sections/GridList/GridList";
+import Message from '@/components/ui/Message/Message';
 
 // Constants
 import { FAVORITES, FAVORITES_ERROR, FAVORITES_LOADING, FAVORITES_NO } from "@/constants";
 
 export default function FavoritesPage(): JSX.Element {
   const favoritesIds = useSelector(selectFavorites);
-  const [posts, setPosts] = useState<PostType[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (favoritesIds.length === 0) return;
-
-    let isMounted = true;
-
-    const fetchFavorites = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      try {
-        const results = await fetchWebApi.getPostsByIds(favoritesIds);
-
-        if (isMounted) {
-          setPosts(results);
-        }
-      } catch(err) {
-        if (isMounted) {
-          setError(FAVORITES_ERROR);
-          console.error(err);
-        }
-      } finally {
-        if (isMounted) {
-          setIsLoading(false);
-        }
-      }
-    }
-
-    fetchFavorites();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [favoritesIds]);
+  const {
+    data: posts = [],
+    isLoading,
+    isError
+  } = useGetFavoritesQuery(favoritesIds, {
+    skip: favoritesIds.length === 0,
+  });
 
   return (
-    <main className="page__content">
+    <main className="page-main">
       <div className="wrapper">
         <SectionTitle text={FAVORITES} />
 
-        {isLoading && (
-          <p>{FAVORITES_LOADING}</p>
-        )}
+        <div className="page-content">
+          {isLoading && (
+            <Message text={FAVORITES_LOADING} />
+          )}
 
-        {error && (
-          <p className="error">{error}</p>
-        )}
+          {isError && (
+            <Message text={FAVORITES_ERROR} />
+          )}
 
-        {favoritesIds.length === 0 ? (
-          <p>{FAVORITES_NO}</p>
-        ) : (
-          <GridList posts={posts} />
-        )}
+          {favoritesIds.length === 0 ? (
+            <Message text={FAVORITES_NO} />
+          ) : (
+            <GridList posts={posts} />
+          )}
+        </div>
       </div>
     </main>
   )
