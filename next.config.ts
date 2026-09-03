e3b6295c17d668cc004a4e4603ecbdb316c2e905
Leonardo Funez <leonardofunezdelchiaro@gmail.com>
2026-09-03 15:13:51 -0500

    Add rewrite async method for api

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://dev-today-news.pantheonsite.io/wp-json/wp/v2/:path*',
+      },
+    ];
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
