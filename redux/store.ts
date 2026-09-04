d41e18edb047087a65c81724cf790d627d02859a
Leonardo Funez <leonardofunezdelchiaro@gmail.com>
2026-09-04 15:02:02 -0500

    AI - add wpApi middleware and reducer to Redux store

--- redux/store.ts
+++ redux/store.ts
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import favoritesReducer from "./slices/favoritesSlice";
 import { favoritesApi } from "./api/favoritesApi";
+import { wpApi } from "./api/wpApi";
 
 export const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
-    'favoritesAPI': favoritesApi.reducer
+    'favoritesAPI': favoritesApi.reducer,
+    'wpAPI': wpApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
-    .concat([favoritesApi.middleware])
+    .concat([favoritesApi.middleware, wpApi.middleware])
 });
 
 setupListeners(store.dispatch)
